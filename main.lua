2292163bd2534f4a10f0dd018d53106436ed8513
Munkh-Od <eikyuu.hoshi@gmail.com>
2020-08-14 16:32:34 +0800

    Added Pong Design , love.graphics.newFont , setFont , clear , rectangle , setColor

--- main.lua
+++ main.lua
@@ -11,6 +11,10 @@ function love.load()
   -- nearest-neighbor filtering on upscaling and downscaling to prevent blurring of text and graphics
   love.graphics.setDefaultFilter('nearest','nearest')
 
+  small_font = love.graphics.newFont('font.ttf',8)
+
+  love.graphics.setFont(small_font)
+
 
   push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH,WINDOW_HEIGHT,{
     fullscreen = false,
@@ -34,13 +38,16 @@ function love.draw()
   
   push:apply('start')
 
-  love.graphics.printf(
-    'Hello Pong',
-    0,
-    VIRTUAL_HEIGHT / 2 - 6,
-    VIRTUAL_WIDTH,
-    'center'
-  )
+  love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
+  -- First Paddle
+  love.graphics.rectangle('fill',10,30,5,20)
+
+  -- Second Paddle
+  love.graphics.rectangle('fill',VIRTUAL_WIDTH-10,VIRTUAL_HEIGHT-50,5,20)
+
+  -- Render Ball
+  love.graphics.rectangle('fill',VIRTUAL_WIDTH/2-2,VIRTUAL_HEIGHT/2-2,4,4)
 
   push:apply('end')
 
