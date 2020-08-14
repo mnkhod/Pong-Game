2d591bd349b89f3cab1324c6180fa31ddc366a72
Munkh-Od <eikyuu.hoshi@gmail.com>
2020-08-14 15:47:38 +0800

    Added Push Library for Virtual Resolution and Texture Filtering Functionality

--- main.lua
+++ main.lua
@@ -1,27 +1,48 @@
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 function love.load()
-  love.window.setMode(WINDOW_WIDTH,WINDOW_HEIGHT,{
+  
+  -- nearest-neighbor filtering on upscaling and downscaling to prevent blurring of text and graphics
+  love.graphics.setDefaultFilter('nearest','nearest')
+
+
+  push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH,WINDOW_HEIGHT,{
     fullscreen = false,
     resizable = false,
     vsync = true
   })
+
 end
 
 
-function love.draw()
-  love.graphics.printf(
-    'Hello Pong',
-    0,
-    WINDOW_HEIGHT / 2 - 6,
-    WINDOW_WIDTH,
-    'center'
-  )
-end
 
 function love.keypressed(key)
   if key == 'escape' then
     love.event.quit()
   end
 end
+
+
+
+function love.draw()
+  
+  push:apply('start')
+
+  love.graphics.printf(
+    'Hello Pong',
+    0,
+    VIRTUAL_HEIGHT / 2 - 6,
+    VIRTUAL_WIDTH,
+    'center'
+  )
+
+  push:apply('end')
+
+end
+
